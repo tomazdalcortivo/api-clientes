4e6fc5114f97437c0b0c58885d2fc6f3d6f8700d
Pedro Tomaz D. Cortivo <159481288+tomazdalcortivo@users.noreply.github.com>
2024-12-06 16:53:14 -0300

    Add files via upload

---
+++ listener-cliente/src/main/java/br/com/rd/listener_cliente/ListenerClienteApplication.java
@@ -0,0 +1,13 @@
+package br.com.rd.listener_cliente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListenerClienteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListenerClienteApplication.class, args);
+	}
+
+}
---
+++ listener-cliente/src/test/java/br/com/rd/listener_cliente/ListenerClienteApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rd.listener_cliente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListenerClienteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
